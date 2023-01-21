8f38c582cee5fc5fcb1070a7ea5c200d7ee6e785
Hardik Bhimani <96721650+hardikbhimani1@users.noreply.github.com>
2023-01-21 22:52:35 +0530

    Soket Io ChatApp

---
+++ Soket.io ChatApp/public/client.js
@@ -0,0 +1,43 @@
+const socket = io("http://localhost:8005")
+
+const form = document.getElementById("send-container")
+const messageInput = document.getElementById("messageInp")
+const messageContainer = document.querySelector('.chat__section')
+var audio = new Audio('MessageTone.mp3')
+
+const append = (message, position)=>{
+    const messageElement = document.createElement("div")
+     messageElement.innerText = (message)
+     messageElement.classList.add('message')
+     messageElement.classList.add(position)
+     messageContainer.append(messageElement)
+     if(position == 'left'){
+         audio.play();
+         }
+
+}
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You:${message}`,'right')
+    socket.emit('send',message)
+    messageInput.value = ''
+})
+const name = prompt("Ple Enter Your Name")
+socket.emit('New-user-joined',name)
+
+socket.on('user-joined',name =>{
+        append(`${name} Join The Chat`,'right')
+})
+
+socket.on('receive',data =>{
+    console.log(data)
+    console.log("data.message",data.message)
+    console.log("data.user",data.user)
+    append(`${data.name}:${data.message}`,'left')
+})
+
+socket.on('left',name =>{
+    append(`${name} Left The Chat`,'right')
+})
+
